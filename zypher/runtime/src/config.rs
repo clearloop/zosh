e8e3050ed63057ddd5669c10a718624f49d7ee2f
clearloop <tianyi.gc@gmail.com>
2025-11-20 22:49:14 +0800

    ci(main): introduce rust ci

--- zypher/runtime/src/config.rs
+++ zypher/runtime/src/config.rs
@@ -1,27 +1,6 @@
 //! Configuration of a ZypherBridge client
 
 use serde::{Deserialize, Serialize};
-use std::net::SocketAddr;
-
-/// Configuration of a ZypherBridge client
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-pub struct Config {
-    /// rpc configuration
-    pub rpc: Rpc,
-
-    /// key configuration
-    pub key: Key,
-}
-
-/// RPC configuration
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-pub struct Rpc {
-    /// solana RPC address
-    pub solana: SocketAddr,
-
-    /// zcash RPC address
-    pub zcash: SocketAddr,
-}
 
 /// Key configuration of a ZypherBridge client
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
