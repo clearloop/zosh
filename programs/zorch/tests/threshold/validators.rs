c957a5c0ca1f282b08a8beb9264effccecf5dacd
clearloop <tianyi.gc@gmail.com>
2025-11-26 19:02:28 +0800

    feat(sol): introduce tests for ed25519

--- programs/zorch/tests/threshold/validators.rs
+++ programs/zorch/tests/threshold/validators.rs
@@ -3,9 +3,21 @@
 use crate::Test;
 use zorch::api;
 
+#[ignore]
 #[test]
 fn test_validators_update() {
     let test = Test::new();
+
+    // 1. initialize the bridge state
+    {
+        let instruction = api::initialize(test.payer, vec![test.payer], 1);
+        let result = test
+            .mollusk
+            .process_instruction(&instruction, &test.initialize_accounts());
+        assert!(!result.program_result.is_err());
+    }
+
+    // 2. update the validator set
     let (signature, sig_instruction) = test.validators_signatures(0, vec![test.payer], 1);
     let instruction = api::validators(test.payer, vec![test.payer], 1, vec![signature]);
     let result = test
