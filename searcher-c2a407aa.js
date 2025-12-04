676b05350cac17429cadbfd72554272e14404350
clearloop <clearloop@users.noreply.github.com>
2025-12-04 13:58:00 +0000

    deploy: d4d6421ae58c96d62eb1c41dfa24aafb0f22fb22

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-4df26cd7.js',
+                path_to_root + 'searchindex-a4ca29d1.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
